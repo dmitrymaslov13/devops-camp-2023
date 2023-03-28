9499257cd67be5079690907364694369c2afcd53
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-03-28 23:00:41 +0500

    Add kustomize generator T-2

---
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -0,0 +1,47 @@
+if [[ $# -eq 0 ]]; then
+   echo args passed
+   exit 0
+fi
+
+folder=repos
+outputFilePath=${folder}/kustomization.yaml
+
+if [[ -d ${folder} ]]; then
+  rm -rf ${folder} &> /dev/null
+fi
+
+mkdir -p ${folder}
+
+cat <<EOF > ${outputFilePath}
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+generatorOptions:
+  disableNameSuffixHash: true
+secretGenerator:
+EOF
+
+# arg1 Name
+# return secretGenerator with name
+function getSecretGenerator {
+cat <<EOF
+  - name: $1
+    namespace: argo-cd
+    options:
+      labels:
+        argocd.argoproj.io/secret-type: repository
+    literals:
+      - name=$1
+      - url=git@github.com:saritasa-nest/$1.git
+      - type=git
+      - project=default
+    files:
+      - sshPrivateKey=$1-deploy-key.pem
+EOF
+}
+
+for name in $*; do
+  ssh-keygen -t ed25519 -N '' -f ${folder}/$name-deploy-key.pem > /dev/null
+  getSecretGenerator $name >> ${outputFilePath}
+done
+
+kubectl kustomize ${folder}
