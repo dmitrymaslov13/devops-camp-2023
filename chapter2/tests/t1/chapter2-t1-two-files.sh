c78d63bca82fd3b9d6c815499afba2de76fd4154
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-03-28 23:21:29 +0500

    Add doc and ouput folder T-1

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -1,12 +1,24 @@
+output=output
+mkdir -p ${output}
+
+# Create file with base64 string with 700 permission
+# arg1 path
 function createFile {
-  echo $RANDOM | base64 > $1
-  chmod 700 $1
+  local out=./${output}/$1
+  echo $RANDOM | base64 > ${out}
+  chmod 700 ${out}
 }
 
+# Get file name from path
+# arg1 path
+function getFileName {
+  echo "$1"| sed "s|.*\/||"
+}
+
+# Show file content
 function showFile {
-  echo $1
   echo ===
-  cat $1
+  echo $1
 }
 
 for i in $*; do
@@ -14,6 +26,6 @@ for i in $*; do
   then
     showFile $1
   else
-    createFile $1
+    createFile $(getFileName $1)
   fi
 done
