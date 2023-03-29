784100474d27a914bdf451a6309160390966accc
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-03-29 09:59:42 +0500

    Update doc and naming T-2

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -1,17 +1,41 @@
 if [[ $# -eq 0 ]]; then
-   echo args passed
-   exit 0
+  echo this script requires 1 or more arguments
+  echo argument - configuration name
+  exit 0
 fi
 
 folder=repos
 outputFilePath=${folder}/kustomization.yaml
 
-if [[ -d ${folder} ]]; then
-  rm -rf ${folder} &> /dev/null
-fi
+# Clean workspace
+clean_workspace() {
+  rm -rf ${folder} && mkdir ${folder}
+}
 
-mkdir -p ${folder}
+# Get secret generator.
+# arg1 Repo name.
+# return secretGenerator with name.
+get_secret_generator() {
+local repo_name=$1
+cat <<EOF
+  - name: $repo_name
+    namespace: argo-cd
+    options:
+      labels:
+        argocd.argoproj.io/secret-type: repository
+    literals:
+      - name=$repo_name
+      - url=git@github.com:saritasa-nest/$repo_name.git
+      - type=git
+      - project=default
+    files:
+      - sshPrivateKey=$repo_name-deploy-key.pem
+EOF
+}
 
+clean_workspace
+
+# Write write non-reusable configuration to header for kustomization file.
 cat <<EOF > ${outputFilePath}
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
@@ -20,28 +44,9 @@ generatorOptions:
 secretGenerator:
 EOF
 
-# arg1 Name
-# return secretGenerator with name
-function getSecretGenerator {
-cat <<EOF
-  - name: $1
-    namespace: argo-cd
-    options:
-      labels:
-        argocd.argoproj.io/secret-type: repository
-    literals:
-      - name=$1
-      - url=git@github.com:saritasa-nest/$1.git
-      - type=git
-      - project=default
-    files:
-      - sshPrivateKey=$1-deploy-key.pem
-EOF
-}
-
 for name in $*; do
   ssh-keygen -t ed25519 -N '' -f ${folder}/$name-deploy-key.pem > /dev/null
-  getSecretGenerator $name >> ${outputFilePath}
+  get_secret_generator $name >> ${outputFilePath}
 done
 
 kubectl kustomize ${folder}
