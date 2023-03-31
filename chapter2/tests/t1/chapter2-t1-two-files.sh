a37df09847c60222abe1cee9ced38df4de58ab9a
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-03-31 19:16:50 +0500

    Format the script file and add doc T1

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -1,35 +1,55 @@
 #!/bin/bash
 
-output=output
-mkdir -p ${output}
+# Constants
+readonly OUTPUT_FOLDER_PATH="./output"
+readonly PATHS=("$@")
 
-# Create file with base64 string with 700 permission
-# arg1 path
-function createFile {
-  local out=./${output}/$1
-  head -n 10 /dev/urandom | base64 > ${out}
-  chmod 700 ${out}
+########################################
+# Create file with random base64 string .
+# Arguments:
+#   Path to file.
+# Returns:
+#   None
+########################################
+function create_file {
+  declare -r OUTPUT_FILE_PATH="./${OUTPUT_FOLDER_PATH}/$1"
+  head -n 10 /dev/urandom | base64 > "${OUTPUT_FILE_PATH}"
+  chmod 700 "${OUTPUT_FILE_PATH}"
 }
 
-# Get file name from path
-# arg1 path
-function getFileName {
-  echo "$1"| sed "s|.*\/||"
+########################################
+# Get file name from path.
+# Arguments:
+#   Path to file.
+# Outputs:
+#   Writes filename to stdout
+########################################
+get_file_name() {
+  declare -r FILE_PATH="$1"
+  echo "${FILE_PATH}" | sed -e "s|.*\/||"
 }
 
-# Show file content
-# arg1 path to file
-function showFile {
-  echo $1
-  echo ===
-  cat $1
+########################################
+# Show file content.
+# Arguments:
+#   Path to file.
+# Outputs:
+#   Writes filename and file content to stdout
+########################################
+show_file_content() {
+  declare -r FILE_PATH="$1"
+  echo "${FILE_PATH}"
+  echo "======================="
+  cat "${FILE_PATH}"
 }
 
-for i in $*; do
-  if [[ -e $i ]] 
-  then
-    showFile $i
+mkdir -p ${OUTPUT_FOLDER_PATH}
+
+for file_path in ${#PATHS[@]}; do
+  get_file_name "${file_path}"
+  if [[ -e ${file_path} ]]; then
+    show_file_content "${file_path}"
   else
-    createFile $(getFileName $i)
+    create_file "$(getFileName "${file_path}")"
   fi
 done
