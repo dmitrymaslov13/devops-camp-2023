974ab3a697610964ebed7c0a853ba9bdc303bca2
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-04-03 22:22:42 +0500

    Add script T5

---
+++ chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+IFS=$'\n'
+readonly DIRECTORY_PATH="$1"
+readonly FILE_PATHES=( $( find "${DIRECTORY_PATH}" -type f ) )
+readonly FILE_NAMES="${FILE_PATHES[*]##*/}"
+
+###################################################
+# Get file name without extenstion
+# Globals:
+#   None
+# Arguments:
+#   File name
+# Outputs:
+#   Writes file name without extenstion to stdout
+###################################################
+get_file_extenstion() {
+  local file_name="${1}" 
+  if [[ "${file_name: 0 : 1 }" != "." && ${file_name} == *.*  ]]; then
+    echo "${file_name##*.}"
+  fi
+}
+
+(
+  for file_path in ${FILE_NAMES}; do
+    get_file_extenstion "${file_path}"
+  done
+) | uniq
