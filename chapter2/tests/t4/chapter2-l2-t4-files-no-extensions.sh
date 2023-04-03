7c4a7952d3b681e039fbf554f208be70f76fdf6c
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-04-03 20:00:40 +0500

    Add show file functional with dot T-4

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -4,6 +4,30 @@ IFS=$'\n'
 readonly DIRECTORY_PATH="$1"
 readonly FILES=( $( find "${DIRECTORY_PATH}" -type f ) )
 
-for file in "${FILES[@]}"; do
-  echo "${file%.*}"
+
+get_file_name_without_extenstion() {
+  local file_name="${1}" 
+  local file_name_without_first_symbol="${file_name: 1 : ${#file_name} }"
+  echo "${file_name: 0: 1}${file_name_without_first_symbol%.*}"
+}
+
+get_file_dir() {
+  local file_path="${1}"
+  echo "${file_path%/*}"
+}
+
+get_file_name() {
+  local file_path="${1}"
+  echo "${file_path##*/}"
+}
+
+get_file_name_path_without_extenstion() {
+  local file_path="${1}"
+  local file_dir=$( get_file_dir ${file_path} )
+  local file_name=$( get_file_name ${file_path} )
+  echo "${file_dir}/$( get_file_name_without_extenstion ${file_name} )"
+}
+
+for file_path in "${FILES[@]}"; do
+  echo $( get_file_name_path_without_extenstion "${file_path}" )
 done
