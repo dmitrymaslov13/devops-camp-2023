23a7f359ab22365c5e9dd0ba1d7fcbbdb9a32181
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-04-04 00:03:07 +0500

    Add space T6

--- chapter2/tests/t6/chapter2-l2-t6-get-file-unique-dirs.sh
+++ chapter2/tests/t6/chapter2-l2-t6-get-file-unique-dirs.sh
@@ -3,10 +3,10 @@
 IFS=$'\n'
 readonly DIRECTORY_PATH="$1"
 readonly FILE_PATHES=( $( find "${DIRECTORY_PATH}" -type f ) )
-readonly DIR_WITH_FILE_PATHES="${FILE_PATHES[@]%/*}"
+readonly DIR_WITH_FILE_PATHES="${FILE_PATHES[*]%/*}"
 
 (
   for dir_path in ${DIR_WITH_FILE_PATHES}; do
     echo "${dir_path}"
   done
-) | uniq -u
+) | sort -u
