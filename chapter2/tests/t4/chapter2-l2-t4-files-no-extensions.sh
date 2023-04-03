3999c1ed262a310dc79861e814ce6a6d06fe0f45
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-04-03 20:12:40 +0500

    Add doc T4

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -4,30 +4,65 @@ IFS=$'\n'
 readonly DIRECTORY_PATH="$1"
 readonly FILES=( $( find "${DIRECTORY_PATH}" -type f ) )
 
-
+###################################################
+# Get file name without extenstion
+# Globals:
+#   None
+# Arguments:
+#   File name
+# Outputs:
+#   Writes file name without extenstion to stdout
+###################################################
 get_file_name_without_extenstion() {
   local file_name="${1}" 
   local file_name_without_first_symbol="${file_name: 1 : ${#file_name} }"
   echo "${file_name: 0: 1}${file_name_without_first_symbol%.*}"
 }
 
-get_file_dir() {
+##################################
+# Get path to file dir
+# Globals:
+#   None
+# Arguments:
+#   Path to file 
+# Outputs:
+#   Writes path to file to stdout
+##################################
+get_path_to_file_dir() {
   local file_path="${1}"
   echo "${file_path%/*}"
 }
 
+####################################
+# Get file name
+# Globals:
+#   None
+# Arguments:
+#   Path to file 
+# Outputs:
+#   Writes file name to stdout
+####################################
 get_file_name() {
   local file_path="${1}"
   echo "${file_path##*/}"
 }
 
-get_file_name_path_without_extenstion() {
+####################################################
+# Get path to file without extenstion 
+# Globals:
+#   None
+# Arguments:
+#   Path to file 
+# Outputs:
+#   Writes path to file without extenstion to stdout
+####################################################
+get_path_to_file_without_extenstion() {
   local file_path="${1}"
-  local file_dir=$( get_file_dir ${file_path} )
+  local file_dir=$( get_path_to_file_dir ${file_path} )
   local file_name=$( get_file_name ${file_path} )
   echo "${file_dir}/$( get_file_name_without_extenstion ${file_name} )"
 }
 
 for file_path in "${FILES[@]}"; do
-  echo $( get_file_name_path_without_extenstion "${file_path}" )
+  echo $( get_path_to_file_without_extenstion "${file_path}" )
 done
