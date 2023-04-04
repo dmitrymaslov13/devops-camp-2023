42375badfca99552b04edb500a665319bd33965f
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-04-04 11:54:14 +0500

    Add script T3

--- chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
+++ chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
-get_namespaces_from_cluster() {
-  curl https://gist.githubusercontent.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/0e57e62984b69ff16c9b9159776e145f6d4feecf/kubernetes-namespaces.txt
+readonly BACKUP_MANIFEST_URL='https://gist.githubusercontent.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/030a82af2489bc353cec370779cfd06852668e29/velero.yaml'
+readonly KUBE_NAMESPACES='https://gist.githubusercontent.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/0e57e62984b69ff16c9b9159776e145f6d4feecf/kubernetes-namespaces.txt'
+
+#########################################
+# Get backup manifest 
+# Globals:
+#   BACKUP_MANIFEST_URL
+# Outputs:
+#   Backup manifest
+#########################################
+get_backup_manifest() {
+  curl --silent "${BACKUP_MANIFEST_URL}"
 }
 
-get_backup_file() {
-  curl https://gist.githubusercontent.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/030a82af2489bc353cec370779cfd06852668e29/velero.yaml
+#########################################
+# Get backup manifest 
+# Arguments:
+#   Backup mainfest
+# Outputs:
+#   Included namespaces
+#########################################
+get_included_namespaces_from_backup() {
+  local backup="${1}"
+  echo "${backup}"| yq ".spec.source.helm.values" -r | yq '.schedules[].template.includedNamespaces[]' -r
 }
 
-get_namespaces_from_backup() {
-  cat "$1"
+#########################################
+# Get kubernetes namespaces
+# Globals:
+#   KUBE_NAMESPACES 
+# Outputs:
+#   Kubernetes namespaces
+#########################################
+get_kube_namespaces() {
+  curl --silent "${KUBE_NAMESPACES}"
 }
 
-get_backup_file > backupfile.yaml
-get_namespaces_from_cluster > namespaces
+namespaces_from_backup=$( get_included_namespaces_from_backup "$( get_backup_manifest )" )
+kube_namespaces=$( get_kube_namespaces )
 
-# schedules.[].includedNamespaces
+(
+  echo "${namespaces_from_backup}"
+  echo "${kube_namespaces}"
+) | sort -u | tr " " "\n"
