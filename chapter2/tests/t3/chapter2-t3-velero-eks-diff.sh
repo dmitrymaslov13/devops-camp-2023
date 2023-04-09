2091f85fcfbe65565c5660a95e524517a9402be5
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-04-09 13:13:58 +0500

    Adjust error handling T3

--- chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
+++ chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-readonly BACKUP_MANIFEST_URL='https://gist.githubusercontent.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/030a82af2489bc353cec370779cfd06852668e29/velero.yaml'
-readonly KUBE_NAMESPACES='https://gist.githubusercontent.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/0e57e62984b69ff16c9b9159776e145f6d4feecf/kubernetes-namespaces.txt'
+set -eou pipefail
 
-#########################################
-# Get backup manifest 
-# Globals:
-#   BACKUP_MANIFEST_URL
-# Outputs:
-#   Backup manifest
-#########################################
-get_backup_manifest() {
-  curl --silent "${BACKUP_MANIFEST_URL}"
-}
+readonly BACKUP_MANIFEST_URL='https://gist.githubusercontent.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/030a82af2489bc353cec370779cfd06852668e29/velero.yaml'
+readonly KUBE_NAMESPACES_URL='https://gist.githubusercontent.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/0e57e62984b69ff16c9b9159776e145f6d4feecf/kubernetes-namespaces.txt'
+
 
 #########################################
 # Get backup manifest 
@@ -23,24 +15,31 @@ get_backup_manifest() {
 #########################################
 get_included_namespaces_from_backup() {
   local backup="${1}"
-  echo "${backup}"| yq ".spec.source.helm.values" -r | yq '.schedules[].template.includedNamespaces[]' -r
+  echo "${backup}"| yq ".spec.source.helm.values" -r | yq ".schedules[].template.includedNamespaces[]" -r
 }
 
-#########################################
-# Get kubernetes namespaces
-# Globals:
-#   KUBE_NAMESPACES 
+###############################################
+# Fetch data.
+# Arguments:
+#  URL 
 # Outputs:
-#   Kubernetes namespaces
-#########################################
-get_kube_namespaces() {
-  curl --silent "${KUBE_NAMESPACES}"
+#   Fetch result or or terminates the script 
+###############################################
+fetch_data() {
+  local url="$1"
+  local response_status=$(curl -I --silent "${url}" | head -n 1 | cut -d' ' -f2)
+
+  if [[ "${response_status}" != 200 ]]; then
+    echo "Fetch error. Url with problem: ${url}. Status code:${response_status}" >&2;
+    exit 1;
+  fi
+
+  curl --silent "${url}"
 }
 
-namespaces_from_backup=$( get_included_namespaces_from_backup "$( get_backup_manifest )" )
-kube_namespaces=$( get_kube_namespaces )
+backup_manifest=$(fetch_data "${BACKUP_MANIFEST_URL}" )
+namespaces_from_backup=$( get_included_namespaces_from_backup "${backup_manifest}" )
 
-(
-  echo "${namespaces_from_backup}"
-  echo "${kube_namespaces}"
-) | sort -u | tr " " "\n"
+kube_namespaces=$(fetch_data "${KUBE_NAMESPACES_URL}")
+
+comm -13 <( echo "${namespaces_from_backup}" | sort -u) <( echo "${kube_namespaces}" | sort -u) 
