baf635a54064f3e5cf442906eae1beb92eca68ea
Dmitry Maslov <dmitry.maslov@saritasa.com>
2023-04-09 14:05:27 +0500

    Remove extra breakest T6

--- chapter2/tests/t6/chapter2-l2-t6-get-file-unique-dirs.sh
+++ chapter2/tests/t6/chapter2-l2-t6-get-file-unique-dirs.sh
@@ -2,11 +2,8 @@
 
 IFS=$'\n'
 readonly DIRECTORY_PATH="$1"
-readonly FILE_PATHES=( $( find "${DIRECTORY_PATH}" -type f ) )
-readonly DIR_WITH_FILE_PATHES="${FILE_PATHES[*]%/*}"
+readonly FILE_PATHES=$(find "${DIRECTORY_PATH}" -type f)
 
-(
-  for dir_path in ${DIR_WITH_FILE_PATHES}; do
-    echo "${dir_path}"
-  done
-) | sort -u
+for file_path in ${FILE_PATHES}; do
+  echo "${file_path%/*}"
+done | sort -u
